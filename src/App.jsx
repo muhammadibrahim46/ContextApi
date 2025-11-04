5c3ef6a9911de25a71883e3ac8bb85c2007f8d10
Ibrahimfridi <ibrahimafridi446644@gmail.com>
2025-11-04 15:17:29 +0500

    update file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './component/card'
+import ThemeBtn from './component/themBtn'
+import './component/index.css'
+function App() {
+  return (
+    <div>
+      <Card />
+      <ThemeBtn />
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import khan from './spotyfi.jpg'
+function Card() {
+  return (
+    <div className='khan'>
+      <img  src={khan} alt="Khan" />
+      <h1>apple watch series 7 gps ,aluminum case, <br />starlight sport 349-9382</h1>
+    </div>
+  )
+}
+
+export default Card
